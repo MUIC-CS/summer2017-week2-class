06c6b78e5134801359703c3ff651683833ecca03
Piti <piti118@gmail.com>
2017-08-09 12:00:24 +0700

    update

--- phonerecord.py
+++ phonerecord.py
@@ -1,6 +1,35 @@
+from db import get_cursor
+
+
 class PhoneRecord:
 
     def __init__(self, id, name, phoneno):
-        this.id = id
-        this.name = name
-        this.phoneno = phoneno
+        self.id = id
+        self.name = name
+        self.phoneno = phoneno
+
+
+class PhoneRecordRepo:
+
+    @classmethod  # static method
+    def create_table(cls):
+        with get_cursor() as cur:
+            cur.execute(
+                """
+                DROP TABLE IF EXISTS phonerecords;
+                CREATE TABLE phonerecords (
+                    id SERIAL NOT NULL PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    phoneno TEXT NOT NULL
+                );
+                """
+            )
+            cur.connection.commit()
+
+if __name__ == '__main__':
+    PhoneRecordRepo.create_table()
+#####
+####
+####
+####
+#####
