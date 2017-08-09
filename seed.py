e724ab079ac4f3f9902a09bf5059c27f4599a75a
Piti <piti118@gmail.com>
2017-08-09 15:05:23 +0700

    update

---
+++ seed.py
@@ -0,0 +1,23 @@
+from phonerecord import PhoneRecord, PhoneRecordRepo
+from friendship import FriendShip, FriendShipRepo
+
+if __name__ == '__main__':
+    PhoneRecordRepo.create_table()
+
+    kfc = PhoneRecord(None, 'KFC', '1150')
+    mcdonald = PhoneRecord(None, 'McDonald', '1711')
+    pizza = PhoneRecord(None, 'Pizza Company', '1112')
+    chester = PhoneRecord(None, 'Chester Grill', '1145')
+
+    PhoneRecordRepo.add(kfc)
+    PhoneRecordRepo.add(mcdonald)
+    PhoneRecordRepo.add(pizza)
+    PhoneRecordRepo.add(chester)
+
+    FriendShipRepo.add_friendship(kfc.id, pizza.id, True)
+    FriendShipRepo.add_friendship(kfc.id, chester.id, False)
+    FriendShipRepo.add_friendship(chester.id, mcdonald.id, False)
+    FriendShipRepo.add_friendship(kfc.id, mcdonald.id, False)
+
+    all = PhoneRecordRepo.find_all()
+    print all
